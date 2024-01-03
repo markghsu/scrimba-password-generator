5f8e0e59f6a25c8c6e5c282dc1ac44e0b9c5bc54
Mark Hsu <mark.g.hsu@gmail.com>
2024-01-02 23:41:00 -0800

    Completed stretch goals: - set password length via input field - toggle symbols via styled checkbox - copy on click - light/dark theming via prefers-color-scheme media query

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Password Generator for Scrimba Front-End Course
 - ☒ Generate two random passwords when user clicks button
 - ☒ Each password should be 15 characters long and generate from charset: A-Z,a-z,0-9,~,\`,!,@,#,$,%,^,&,\*,(,),\_,-,+,=,\{,\[,},\],",",\|,:,;,<,>,.,\?,/
 
-- ☐ Set password length
-- ☐ copy-on-click
-- ☐ Toggle Symbols/numbers
-- ☐ Light/Dark Theme
+- ☒ Set password length
+- ☒ copy-on-click
+- ☒ Toggle Symbols/numbers
+- ☒ Light/Dark Theme
--- index.js
+++ index.js
@@ -2,22 +2,43 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
 
 const pw1El = document.getElementById('pw1');
 const pw2El = document.getElementById('pw2');
+const allowSpecEl = document.getElementById('allow-special');
+const numEl = document.getElementById('num-chars');
+const noteEl = document.getElementById('notification');
 document.getElementById('generate-btn').addEventListener('click', generatePasswords);
+pw1El.addEventListener('click', copyMe);
+pw2El.addEventListener('click', copyMe);
 
 function generatePasswords() {
-	pw1El.value = genRandomPass(15,true);
-	pw2El.value = genRandomPass(15,true);
+    let num = numEl.value;
+    if(Number.isNaN(num) || num <= 0 || num > 32) {
+        numEl.value = 15;
+        num = 15;
+    }
+    pw1El.value = genRandomPass(num,allowSpecEl.checked);
+    pw2El.value = genRandomPass(num,allowSpecEl.checked);
+    noteEl.textContent = "Passwords generated!";
 }
 
 
 function genRandomPass(len,includeSpecial) {
-	let rstr = '';
-	for(let i = 0; i < len; i++) {
-		if(includeSpecial) {
-			rstr += characters[Math.floor(Math.random()*characters.length)];
-		} else {
-			rstr += characters[Math.floor(Math.random()*62)]; // A-Z, a-z, 0-9
-		}
-	}
-	return rstr;
+    let rstr = '';
+    for(let i = 0; i < len; i++) {
+        if(includeSpecial) {
+            rstr += characters[Math.floor(Math.random()*characters.length)];
+        } else {
+            rstr += characters[Math.floor(Math.random()*62)]; // A-Z, a-z, 0-9
+        }
+    }
+    return rstr;
+}
+
+
+function copyMe(evt) {
+    const ctarget = evt.target;
+    console.log(evt.target.value);
+    ctarget.select();
+    ctarget.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(ctarget.value);
+    noteEl.textContent = "Password copied!";
 }
